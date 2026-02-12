675217afbb91f12d8f70a37863702001f9672324
Prakhar Shukla <prakharshukla297@gmail.com>
2026-02-12 22:01:32 +0530

    Day -1 of JavaScripts

---
+++ Basics/datatypes.js
@@ -0,0 +1,5 @@
+console.log(typeof "Hi");     // string
+console.log(typeof true);     // boolean
+console.log(typeof []);       // object 😮
+console.log(typeof {});       // object
+console.log(typeof null);     // object (JS bug)
---
+++ Basics/variable.js
@@ -0,0 +1,40 @@
+const accountId = 12345;
+let accountEmail="prakharshukla297@gmail.com";
+var accountPassword="12345";
+accountCity="Ghaziabad";
+
+// accountId=54321;noot allowed because accountId is a constant
+console.log(accountId);
+accountEmail="prakharshukla7@gmail.com";
+console.log(accountEmail);
+accountPassword="54321";
+console.log(accountPassword);
+accountCity="Noida";
+console.log(accountCity);   
+
+
+// example of let variable
+
+let y = 10;
+// let y = 20; ❌ Error
+y = 25;        // ✅ Allowed
+
+console.log(y); // 25
+
+
+
+// example of const variable
+const pi = 3.14;
+// pi = 3.15; ❌ Error
+
+console.log(pi);
+
+
+
+// example of var variable
+
+var x = 10;
+var x = 20;   // Allowed
+x = 30;       // Allowed
+
+console.log(x); // 30
